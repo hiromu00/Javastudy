690ecf3fd0a82d767574eb2434abcb62ed4330d3
hirom <hiromu01020@gmail.com>
2020-04-04 01:00:31 +0900

    メインクラスのルールを定義

--- src/main.java
+++ src/main.java
@@ -2,7 +2,8 @@
 
 public class main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+//メインクラスは他クラスのメソッドを呼び出すのみにすることが理想
         fizzbuzz.FIZZBUZZ();
     }
 }
